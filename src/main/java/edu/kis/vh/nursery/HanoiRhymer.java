f2bea1a68f52e8e89207f05cdc928cceb8154829
pgates_test2 <bramowskip@gmail.com>
2023-09-14 19:46:31 +0200

    2. Poprawa formatowania pliku HanoiRhymer(2)

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -11,7 +11,7 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
                 totalRejected++;
-            else
+        else
                 super.countIn(in);
-        }
+    }
 }
